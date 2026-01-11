e9f41e16f71915b914b208b6cb240d8d5cc4898b
tamziy <114886626+tamziy@users.noreply.github.com>
2026-01-11 11:12:45 -0600

    Added logging state logic and window size constants

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+
+
 def main():
-    print("Hello from asteroids!")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
