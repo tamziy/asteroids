19fc4f41dc5379b14b43dbc39a66aba44a350076
tamziy <114886626+tamziy@users.noreply.github.com>
2026-01-12 10:52:54 -0600

    Created groups for game objects

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
     time = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
@@ -20,14 +24,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
             
         screen.fill(color="black")
-        player.draw(screen) 
-        player.update(dt)
+        for object in drawable:
+            object.draw(screen)
+        updatable.update(dt)
+
 
         pygame.display.flip()
-        dt = time.tick(144) / 1000
+        dt = time.tick(60) / 1000
         print(dt)
 
 
